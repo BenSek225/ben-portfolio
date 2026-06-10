157b3c3aaa3bdfd4d1b5946351aab0bb858e4452
BenSek225 <sekongobienvenu22@gmail.com>
2026-06-10 18:45:31 +0000

    mise a jour du pixel meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import GlowHeader from "@/components/glow-header"
 import Footer from "@/components/footer"
 import AnimatedBackground from "@/components/animated-background"
 import PageTransition from "@/components/page-transition"
+import Script from "next/script"
 
 const inter = Inter({ subsets: ["latin"] })
 const playfair = Playfair_Display({
@@ -72,26 +73,33 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="fr" className="dark">
-    <head>
-      {/* Meta Pixel Code */}
-      <script>
-        {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1551174763232392');
-          fbq('track', 'PageView');
-        `}
-      </script>
-      <noscript><img height="1" width="1" style={{ display: 'none' } as React.CSSProperties} src="https://www.facebook.com/tr?id=1551174763232392&ev=PageView&noscript=1" /></noscript>
-      {/* End Meta Pixel Code */}
-    </head>
+    <html lang="fr" className="dark"><head>
+    {/* Meta Pixel Code */}
+    <Script id="facebook-pixel" strategy="afterInteractive">
+      {`
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1551174763232392');
+        fbq('track', 'PageView');
+      `}
+    </Script>
+    <noscript>
+      <img 
+        height="1" 
+        width="1" 
+        style={{ display: 'none' }} 
+        src="https://www.facebook.com/tr?id=1551174763232392&ev=PageView&noscript=1" 
+        alt=""
+      />
+    </noscript>
+    {/* End Meta Pixel Code */}
+  </head>
       <body className={`${inter.className} ${playfair.variable}`}>
         <ThemeProvider>
           <div className="relative min-h-screen">
