257e5da66520caded0d82273ed38fa8008eec038
BenSek225 <sekongobienvenu22@gmail.com>
2026-06-12 22:25:53 +0000

    Ajout du pixel directement dans les pages

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,6 +6,7 @@ import { Mail, Download, Award, Users, Coffee, GraduationCap, Lightbulb } from "
 import AnimatedSection from "@/components/animated-section"
 import AnimatedCounter from "@/components/animated-counter.tsx"
 import AnimatedTimelineLine from "@/components/animated-timeline-line"
+import Script from "next/script"
 
 export default function AboutPage() {
   const stats = [
@@ -129,6 +130,35 @@ export default function AboutPage() {
   }
 
   return (
+    <>
+    <head>
+      {/* Meta Pixel Code */}
+      <Script id="facebook-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1551174763232392');
+          fbq('track', 'PageView');
+          fbq('track', 'Lead');
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=1551174763232392&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
+      {/* End Meta Pixel Code */}
+    </head>
     <main className="min-h-screen py-20 px-6 relative z-10">
       <div className="container mx-auto max-w-6xl">
         {/* Hero Section */}
@@ -510,5 +540,6 @@ export default function AboutPage() {
         </section>
       </div>
     </main>
+    </>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,39 @@ import FeaturedProjects from "@/components/featured-projects"
 import SkillsSection from "@/components/skills-section"
 import TestimonialsSection from "@/components/testimonials-section"
 import CTASection from "@/components/cta-section"
+import Script from "next/script"
 
 export default function HomePage() {
   return (
+    <>
+    <head>
+      {/* Meta Pixel Code */}
+      <Script id="facebook-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1551174763232392');
+          fbq('track', 'PageView');
+          fbq('track', 'ViewContent');
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=1551174763232392&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
+      {/* End Meta Pixel Code */}
+    </head>
     <main className="min-h-screen">
       <EnhancedHeroSection />
       <div className="section-padding-sm">
@@ -21,5 +51,6 @@ export default function HomePage() {
         <CTASection />
       </div>
     </main>
+    </>
   )
 }
